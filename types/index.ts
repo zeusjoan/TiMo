e9929e24dc3aad4dc4f1ca0b8ad72857ce829d8c
zeusjoan <zeusjoan@gmail.com>
2024-12-06 23:28:44 +0000

    Mozna powiedziec ze to pierwsza wersja ver 1.00

---
+++ types/index.ts
@@ -0,0 +1,30 @@
+export interface Overtime {
+    date: string;
+    startTime: string;
+    endTime: string;
+    incidentNumber: string;
+    description: string;
+    duration: number;
+  }
+  
+  export interface Budget {
+    orderNumber: string;
+    supplierNumber: string;
+    documentDate: string;
+    deliveryDate: string;
+    contractNumber: string;
+    capex: number;
+    opex: number;
+    support: number;
+    hourlyRate: number;
+  }
+  
+  export interface TimeEntry {
+    month: string;
+    capexHours: number;
+    opexHours: number;
+    supportHours: number;
+    overtimes: Overtime[];
+    description: string;
+    overtimeHours: number;
+  }
